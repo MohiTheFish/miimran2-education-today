331c69ae65eb6267ac3c6d587b87af5d9e7b3ef3
Muhammed Imran <imranmuhammed2000@gmail.com>
2022-01-14 17:05:26 -0600

    clean out flask files

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Import the required libraries
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
@@ -9,7 +8,6 @@ from flask_cors import CORS
 # Create various application instances
 # Order matters: Initialize SQLAlchemy before Marshmallow
 db = SQLAlchemy()
-migrate = Migrate()
 ma = Marshmallow()
 cors = CORS()
 
@@ -23,7 +21,6 @@ def create_app():
     # Initialize extensions
     # To use the application instances above, instantiate with an application:
     db.init_app(app)
-    migrate.init_app(app, db)
     ma.init_app(app)
     cors.init_app(app)
 
